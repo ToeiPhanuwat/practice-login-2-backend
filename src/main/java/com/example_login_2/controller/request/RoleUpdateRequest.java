9a5f28be599e8c25e19765af86fccdf0d15e294b
Toei Sungharut <102471097+Toei56@users.noreply.github.com>
2024-08-23 17:57:41 +0700

    add unit test of  AdminServiceTest

--- src/main/java/com/example_login_2/controller/request/RoleUpdateRequest.java
+++ src/main/java/com/example_login_2/controller/request/RoleUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.example_login_2.controller.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class RoleUpdateRequest {
 
-    @NotBlank
+    @NotEmpty
     private String role;
 }
